22844dab86aaa3867363cc99065ed9f5fa8361b6
Philipp Conrad <philipp8101@gmail.com>
2025-03-23 00:11:18 +0100

    caret macro

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         board = "nice_nano_v2";
         shield = "corne_%PART% nice_view_adapter nice_view";
 
-        zephyrDepsHash = "sha256-Yxgf1E6yXF+IxF5Z/gNcy+VEBRfnq2v9Fa1WxeeXZJw=";
+        zephyrDepsHash = "sha256-k0BrCX7Pspj6/SSOgKSlDW1jZ3OWTFy5f8Kr+vQJ1LU=";
 
         meta = {
           description = "ZMK firmware";
